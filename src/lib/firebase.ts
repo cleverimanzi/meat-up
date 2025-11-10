fe94d688da1dab2d94a3aac552517c07ba7f2c4f
Imanzi Japhet <japhetimanzi@gmail.com>
2025-11-10 11:45:08 +0000

    use this firebase{// Import the functions you need from the SDKs you nee

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAh92mQ4BvB9A5fQ_kEwJ6sRzO8cY_hI4",
+  apiKey: "AIzaSyDjCRNfIvD5X_l4wOoPTif1WbeyfsJMuf4",
   authDomain: "meat-shop-ece1d.firebaseapp.com",
   projectId: "meat-shop-ece1d",
-  storageBucket: "meat-shop-ece1d.appspot.com",
+  storageBucket: "meat-shop-ece1d.firebasestorage.app",
   messagingSenderId: "823811818123",
   appId: "1:823811818123:web:9dcf1c3d7e8eb4aaca8a8e",
   measurementId: "G-VJES2YQ3RK"
