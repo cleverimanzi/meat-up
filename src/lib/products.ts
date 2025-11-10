d35b7f74e189bba36807e6cad29c0be4921bae16
Imanzi Japhet <japhetimanzi@gmail.com>
2025-11-10 14:08:24 +0000

    remove cow cts and add caw meat

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -59,7 +59,7 @@ export const products: Product[] = [
   },
   {
     id: 'prod_010',
-    name: 'Assorted Cow Cuts',
+    name: 'Cow Meat',
     description: 'A selection of various cuts of high-quality cow meat.',
     price: 18.5,
     imageId: 'cow-meat',
